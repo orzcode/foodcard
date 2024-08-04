2628ac1400a55f24be76b0ced1757bcfec4dfed1
orzcode <71000311+orzcode@users.noreply.github.com>
2024-08-04 15:57:53 +1200

    customize scaffold

---
+++ src/InputPane.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+//import './App.css'
+
+function InputPane() {
+	const [count, setCount] = useState(0)
+  
+	return (
+	  <>
+		<div>
+			Fuck
+		</div>
+	</>
+	)
+	}
+
+export default InputPane
