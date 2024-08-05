f46ecf1e2bf55a83a656c2f0985056599d025d21
orzcode <71000311+orzcode@users.noreply.github.com>
2024-08-06 11:40:56 +1200

    REMOVED GROW-WRAP - SEE PREVIOUS COMMIT IF NEEDED

--- src/components/TextArea.jsx
+++ src/components/TextArea.jsx
@@ -3,29 +3,16 @@ import { useState, useRef } from 'react'
 
 function TextArea() {
 	const [value, setValue] = useState("");
-	const textAreaRef = useRef(null);
-  
-	function auto_height(elem) {
-	//   elem.style.height = "auto";
-	//   elem.style.height = `${elem.scrollHeight}px`;
-
-	  elem.parentNode.dataset.replicatedValue = elem.value;
-	  //elem.style.height = "auto";
-	}
   
 	return (
-
-	<div className="grow-wrap">
 	  <textarea
 		value={value}
 		onChange={(event) => setValue(event.target.value)}
-		onInput={() => auto_height(textAreaRef.current)}
-		ref={textAreaRef}
+		placeholder="Enter text here"
 		className="textarea"
 		rows={3}
-		cols={10}
+		cols={30}
 	  />
-	</div>
 	);
   }
   
