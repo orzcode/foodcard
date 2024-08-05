d4c54bfec18f2e152af7263aaeb74b08d1d2ef93
orzcode <71000311+orzcode@users.noreply.github.com>
2024-08-06 11:35:32 +1200

    update grow-wrap

--- src/components/TextArea.jsx
+++ src/components/TextArea.jsx
@@ -6,19 +6,26 @@ function TextArea() {
 	const textAreaRef = useRef(null);
   
 	function auto_height(elem) {
-	  elem.style.height = "auto";
-	  elem.style.height = `${elem.scrollHeight}px`;
+	//   elem.style.height = "auto";
+	//   elem.style.height = `${elem.scrollHeight}px`;
+
+	  elem.parentNode.dataset.replicatedValue = elem.value;
+	  //elem.style.height = "auto";
 	}
   
 	return (
+
+	<div className="grow-wrap">
 	  <textarea
 		value={value}
 		onChange={(event) => setValue(event.target.value)}
 		onInput={() => auto_height(textAreaRef.current)}
 		ref={textAreaRef}
-		rows="2"
 		className="textarea"
+		rows={3}
+		cols={10}
 	  />
+	</div>
 	);
   }
   
