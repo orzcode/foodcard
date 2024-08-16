4f8f1ae9f5dd4d5311360254db9ed5d400b70bb8
orzcode <71000311+orzcode@users.noreply.github.com>
2024-08-16 18:11:29 +1200

    nicer custom image upload

--- src/components/ImageUpload.jsx
+++ src/components/ImageUpload.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import "../styles/ImageUpload.css";
 
 function ImageUpload(imgStateProps) {
-
   // Handle the file selection
   const handleFileUpload = (event) => {
     const file = event.target.files[0];
@@ -12,12 +12,34 @@ function ImageUpload(imgStateProps) {
   };
 
   return (
-    <div>
-      <input 
-        type="file" 
-        accept="image/*" 
-        onChange={handleFileUpload} 
+    <div className="file-input">
+      <input
+        type="file"
+        accept="image/*;capture=camera"
+        onChange={handleFileUpload}
+        name="file-input"
+        id="file-input"
+        className="file-input__input"
       />
+      <label className="file-input__label" htmlFor="file-input">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="24px"
+          viewBox="0 -960 960 960"
+          width="24px"
+          fill="#e8eaed"
+
+          aria-hidden="true"
+          focusable="false"
+          data-prefix="fas"
+          data-icon="upload"
+          className="svg-inline--fa fa-upload fa-w-16"
+          role="img"
+        >
+          <path d="M480-480ZM200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h320v80H200v560h560v-320h80v320q0 33-23.5 56.5T760-120H200Zm40-160h480L570-480 450-320l-90-120-120 160Zm440-320v-80h-80v-80h80v-80h80v80h80v80h-80v80h-80Z" />
+        </svg>
+        <span>Upload a pic!</span>
+      </label>
     </div>
   );
 }
