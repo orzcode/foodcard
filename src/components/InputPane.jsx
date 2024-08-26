77e81da86f0e746e8427440cc69f95aadf7e1313
orzcode <71000311+orzcode@users.noreply.github.com>
2024-08-26 16:50:54 +1200

    remove react accordion

--- src/components/InputPane.jsx
+++ src/components/InputPane.jsx
@@ -1,84 +1,90 @@
-import React from 'react';
-import InputField from './InputField';
-import TextArea from './TextArea';
-import ImageUpload from './ImageUpload';
-import '../styles/InputPane.css';
+import React from "react";
+import InputField from "./InputField";
+import TextArea from "./TextArea";
+import ImageUpload from "./ImageUpload";
+import "../styles/InputPane.css";
 
 // import { Accordion } from 'rsuite';
 // import 'rsuite/dist/rsuite-no-reset.min.css'
 // https://rsuitejs.com/components/accordion/
 
+//try RCcollapse
+
 function InputPane(props) {
   return (
-    <div className='InputPane'>
+    <div className="InputPane">
       <h2>Recipe Name</h2>
-      <InputField 
-        actionType="apply" 
-        placeholder="Recipe Name" 
-        setRecipe={props.setRecipe} 
-        recipe={props.recipe} 
-        propName="name" 
+      <InputField
+        actionType="apply"
+        placeholder="Recipe Name"
+        setRecipe={props.setRecipe}
+        recipe={props.recipe}
+        propName="name"
         initialValue={props.recipe.name}
       />
 
-      <ImageUpload image={props.image} setImage={props.setImage} preview={props.preview} setPreview={props.setPreview}/>      
+      <ImageUpload
+        image={props.image}
+        setImage={props.setImage}
+        preview={props.preview}
+        setPreview={props.setPreview}
+      />
 
       <h2>Ingredients</h2>
-      <ul>                
+      <ul>
         {/* Note - this of course re-renders once a change occurs
         so the index is only needed here on this re-rendered version, not the 'blank' one below */}
         {props.recipe.ingredients.map((ingredient, index) => (
-          <InputField 
-            key={index} 
-            actionType="apply" 
-            setRecipe={props.setRecipe} 
-            recipe={props.recipe} 
-            propName="ingredients" 
-            initialValue={ingredient} 
+          <InputField
+            key={index}
+            actionType="apply"
+            setRecipe={props.setRecipe}
+            recipe={props.recipe}
+            propName="ingredients"
+            initialValue={ingredient}
             index={index}
           />
         ))}
         {/* Add one empty field at the end */}
-        <InputField 
-          actionType="applyAndClear" 
-          placeholder="Add new ingredient" 
-          setRecipe={props.setRecipe} 
-          recipe={props.recipe} 
+        <InputField
+          actionType="applyAndClear"
+          placeholder="Add new ingredient"
+          setRecipe={props.setRecipe}
+          recipe={props.recipe}
           propName="ingredients"
         />
       </ul>
 
-
       <h2>Instructions</h2>
       <ol>
-      {props.recipe.instructions.map((instruction, index) => (
-          <InputField 
-            key={index} 
-            actionType="apply" 
-            setRecipe={props.setRecipe} 
-            recipe={props.recipe} 
-            propName="instructions" 
-            initialValue={instruction} 
+        {props.recipe.instructions.map((instruction, index) => (
+          <InputField
+            key={index}
+            actionType="apply"
+            setRecipe={props.setRecipe}
+            recipe={props.recipe}
+            propName="instructions"
+            initialValue={instruction}
             index={index}
           />
         ))}
         {/* Add one empty field at the end */}
-        <InputField 
+        <InputField
           actionType="applyAndClear"
-          placeholder="Add instruction" 
-          setRecipe={props.setRecipe} 
-          recipe={props.recipe} 
+          placeholder="Add instruction"
+          setRecipe={props.setRecipe}
+          recipe={props.recipe}
           propName="instructions"
         />
       </ol>
 
       <h2>Notes</h2>
-      <TextArea 
-        actionType="apply" 
-        placeholder="Optional notes" 
-        setRecipe={props.setRecipe} 
-        recipe={props.recipe} 
-        propName="notes" 
+      <TextArea
+        actionType="apply"
+        placeholder="Optional notes"
+        setRecipe={props.setRecipe}
+        recipe={props.recipe}
+        propName="notes"
         initialValue={props.recipe.notes}
       />
     </div>
