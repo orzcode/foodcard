d16076806a161cfcbb3cd4b7dfb3b2e547f9d485
orzcode <71000311+orzcode@users.noreply.github.com>
2024-08-26 18:03:32 +1200

    fixed active transitions
    needs work

--- src/components/InputPane.jsx
+++ src/components/InputPane.jsx
@@ -1,4 +1,4 @@
-import { React, useState } from "react";
+import React, { useState } from "react";
 import InputField from "./InputField";
 import TextArea from "./TextArea";
 import ImageUpload from "./ImageUpload";
@@ -13,122 +13,102 @@ function InputPane(props) {
 
   return (
     <div className="InputPane">
-      <div className="accordion-section">
+      <div className={`accordion-section ${openSection === "name" ? "active" : ""}`}>
         <h2 onClick={() => toggleSection("name")} className="accordion-label">
           Recipe Name
         </h2>
-        {openSection === "name" && (
-          <div className="accordion-content">
-            <InputField
-              actionType="apply"
-              placeholder="Recipe Name"
-              setRecipe={props.setRecipe}
-              recipe={props.recipe}
-              propName="name"
-              initialValue={props.recipe.name}
-            />
-          </div>
-        )}
+        <div className="accordion-content">
+          <InputField
+            actionType="apply"
+            placeholder="Recipe Name"
+            setRecipe={props.setRecipe}
+            recipe={props.recipe}
+            propName="name"
+            initialValue={props.recipe.name}
+          />
+        </div>
       </div>
 
-      <div className="accordion-section">
+      <div className={`accordion-section ${openSection === "image" ? "active" : ""}`}>
         <h2 onClick={() => toggleSection("image")} className="accordion-label">
           Picture
         </h2>
-        {openSection === "image" && (
-          <div className="accordion-content">
-            <ImageUpload
-              image={props.image}
-              setImage={props.setImage}
-              preview={props.preview}
-              setPreview={props.setPreview}
-            />
-          </div>
-        )}
+        <div className="accordion-content">
+          <ImageUpload
+            image={props.image}
+            setImage={props.setImage}
+            preview={props.preview}
+            setPreview={props.setPreview}
+          />
+        </div>
       </div>
 
-      <div className="accordion-section">
-        <h2
-          onClick={() => toggleSection("ingredients")}
-          className="accordion-label"
-        >
+      <div className={`accordion-section ${openSection === "ingredients" ? "active" : ""}`}>
+        <h2 onClick={() => toggleSection("ingredients")} className="accordion-label">
           Ingredients
         </h2>
-        {openSection === "ingredients" && (
-
-            <ul className="accordion-content">
-              {props.recipe.ingredients.map((ingredient, index) => (
-                <InputField
-                  key={index}
-                  actionType="apply"
-                  setRecipe={props.setRecipe}
-                  recipe={props.recipe}
-                  propName="ingredients"
-                  initialValue={ingredient}
-                  index={index}
-                />
-              ))}
-              <InputField
-                actionType="applyAndClear"
-                placeholder="Add new ingredient"
-                setRecipe={props.setRecipe}
-                recipe={props.recipe}
-                propName="ingredients"
-              />
-            </ul>
-
-        )}
+        <ul className="accordion-content">
+          {props.recipe.ingredients.map((ingredient, index) => (
+            <InputField
+              key={index}
+              actionType="apply"
+              setRecipe={props.setRecipe}
+              recipe={props.recipe}
+              propName="ingredients"
+              initialValue={ingredient}
+              index={index}
+            />
+          ))}
+          <InputField
+            actionType="applyAndClear"
+            placeholder="Add new ingredient"
+            setRecipe={props.setRecipe}
+            recipe={props.recipe}
+            propName="ingredients"
+          />
+        </ul>
       </div>
 
-      <div className="accordion-section">
-        <h2
-          onClick={() => toggleSection("instructions")}
-          className="accordion-label"
-        >
+      <div className={`accordion-section ${openSection === "instructions" ? "active" : ""}`}>
+        <h2 onClick={() => toggleSection("instructions")} className="accordion-label">
           Instructions
         </h2>
-        {openSection === "instructions" && (
-
-            <ol className="accordion-content">
-              {props.recipe.instructions.map((instruction, index) => (
-                <InputField
-                  key={index}
-                  actionType="apply"
-                  setRecipe={props.setRecipe}
-                  recipe={props.recipe}
-                  propName="instructions"
-                  initialValue={instruction}
-                  index={index}
-                />
-              ))}
-              <InputField
-                actionType="applyAndClear"
-                placeholder="Add instruction"
-                setRecipe={props.setRecipe}
-                recipe={props.recipe}
-                propName="instructions"
-              />
-            </ol>
-
-        )}
+        <ol className="accordion-content">
+          {props.recipe.instructions.map((instruction, index) => (
+            <InputField
+              key={index}
+              actionType="apply"
+              setRecipe={props.setRecipe}
+              recipe={props.recipe}
+              propName="instructions"
+              initialValue={instruction}
+              index={index}
+            />
+          ))}
+          <InputField
+            actionType="applyAndClear"
+            placeholder="Add instruction"
+            setRecipe={props.setRecipe}
+            recipe={props.recipe}
+            propName="instructions"
+          />
+        </ol>
       </div>
 
-      <div className="accordion-section">
+      <div className={`accordion-section ${openSection === "notes" ? "active" : ""}`}>
         <h2 onClick={() => toggleSection("notes")} className="accordion-label">
           Notes
         </h2>
-        {openSection === "notes" && (
-          <div className="accordion-content">
-            <TextArea
-              actionType="apply"
-              placeholder="Optional notes"
-              setRecipe={props.setRecipe}
-              recipe={props.recipe}
-              propName="notes"
-              initialValue={props.recipe.notes}
-            />
-          </div>
-        )}
+        <div className="accordion-content">
+          <TextArea
+            actionType="apply"
+            placeholder="Optional notes"
+            setRecipe={props.setRecipe}
+            recipe={props.recipe}
+            propName="notes"
+            initialValue={props.recipe.notes}
+          />
+        </div>
       </div>
     </div>
   );
